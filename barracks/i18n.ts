9d582293d2d4fa3a88eea1f97cab4b96c8183351
William Petit <wpetit@cadoles.com>
2025-10-19 15:26:50 +0200

    fix(barracks): translations load path

--- barracks/i18n.ts
+++ barracks/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { BASE_URL } from './util/baseUrl';
 
 
 i18n
@@ -14,6 +15,10 @@ i18n
 
     interpolation: {
       escapeValue: false, // React already escapes values
+    },
+
+    backend: {
+      loadPath: `${BASE_URL}/locales/{{lng}}/{{ns}}.json`,
     },
 
     // Enable debug mode in development
